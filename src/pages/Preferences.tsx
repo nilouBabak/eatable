1f46a909d04c6c8f1f733e8ae3b32a9f05f6a026
atkaczyk <alisa.tk@gmail.com>
2019-04-28 14:36:50 -0400

    page setup file structure pages and component folders installed material ui (core and icons), react router dom set up component menu app bar refinements to follow

---
+++ src/pages/Preferences.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Preferences extends Component {
+  render(){
+    return(
+      <div className="Preferences">
+      This is the Preferences page
+      </div>
+    )
+  }
+}
+
+export default Preferences;
---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SignUp extends Component {
+  render(){
+    return(
+      <div className="SignUp">
+      This is the signup page
+      Please log in or sign up below
+      </div>
+    )
+  }
+}
+
+export default SignUp;
